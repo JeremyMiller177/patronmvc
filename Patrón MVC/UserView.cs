1eb51ca09d6f95b5852f845dfb80cc7668cd3af6
Jeremy Rodriguez <jrodriguezg35@miumg.edu.gt>
2023-05-26 23:34:32 -0600

    Agregar archivos de proyecto.

---
+++ Patrón MVC/UserView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Patrón_MVC
+{
+    internal class UserView
+    {
+        public class UsuarioView
+        {
+            public void ImprimirDetallesUsuario(string nombre, int edad)
+            {
+                Console.WriteLine("Nombre: " + nombre);
+                Console.WriteLine("Edad: " + edad);
+            }
+        }
+
+    }
+}
---
+++ Patrón MVC/Usuario.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Patrón_MVC
+{
+    public class Usuario
+    {
+            public string Nombre { get; set; }
+            public int Edad { get; set; }
+        }
+
+        // Controlador (Controller)
+        public class UsuarioController
+        {
+            private Usuario modelo;
+            private UsuarioView vista;
+
+            public UsuarioController(Usuario modelo, UsuarioView vista)
+            {
+                this.modelo = modelo;
+                this.vista = vista;
+            }
+
+            public void SetNombre(string nombre)
+            {
+                modelo.Nombre = nombre;
+            }
+
+            public void SetEdad(int edad)
+            {
+                modelo.Edad = edad;
+            }
+
+            public string GetNombre()
+            {
+                return modelo.Nombre;
+            }
+
+            public int GetEdad()
+            {
+                return modelo.Edad;
+            }
+
+            public void ActualizarVista()
+            {
+                vista.ImprimirDetallesUsuario(modelo.Nombre, modelo.Edad);
+            }
+        }
+
+        // Vista (View)
+        public class UsuarioView
+        {
+            public void ImprimirDetallesUsuario(string nombre, int edad)
+            {
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Edad: " + edad);
+            }
+        }
+
+        // Clase principal para probar la implementación
+        class Program
+        {
+        public Program()
+        {
+        }
+
+        static void Main(string[] args)
+            {
+                // Crear el modelo
+                Usuario modelo = new Usuario();
+
+                // Crear la vista
+                UsuarioView vista = new UsuarioView();
+
+                // Crear el controlador y pasarle el modelo y la vista
+                UsuarioController controlador = new UsuarioController(modelo, vista);
+
+                // Actualizar el modelo
+                controlador.SetNombre("John Doe");
+                controlador.SetEdad(30);
+
+                // Actualizar la vista con los datos del modelo
+                controlador.ActualizarVista();
+
+              Console.ReadKey();
+            }
+        }
+
+    }
+
